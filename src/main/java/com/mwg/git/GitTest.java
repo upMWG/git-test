c2814e24902c9f005bce4aa03f295f8d08ccccd8
mwg <1073093915@qq.com>
2021-05-16 16:16:44 +0800

    first idea commit

---
+++ src/main/java/com/mwg/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.mwg.git;
+
+/**
+ * @author mwg
+ * @description
+ * @creat 2021-05-16 16:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
